01076a74840306d0f77d97d3ddaea3d7f19bee63
Inyrie <inyrie@web.de>
2013-11-17 17:31:51 +0100

    Update.

--- abgabe 1/aufgabe1fpolyfit.m
+++ abgabe 1/aufgabe1fpolyfit.m
@@ -8,17 +8,21 @@ vectorX = mycheby(n);
 vectorY = evaluateOrigFunct(vectorX);
 vectorYAbs = evaluateAbsolute(vectorX);
 
+% Aufstellen des Interpolations-Polynoms.
 % MATLAB warnt - Polynomial is badly conditioned!
 interpolPolyCoeffs = polyfit(vectorX, vectorY, n-1);
 interpolPolyAbsCoeffs = polyfit(vectorX, vectorYAbs, n-1);
 
-evaluatedYs = zeros(1,length(vectorX));
-
 % Auswerten des Polynoms an ausreichend vielen Stellen fuer schoeneren
 % Graphen:
 xToEvaluate=-1:0.001:1;
+evaluatedYs = zeros(1,length(vectorX));
+evaluatedYsAbs = zeros(1,length(vectorX));
+
 for i=1:length(xToEvaluate)
     evaluatedYs(i) = hornerSolve(interpolPolyCoeffs,xToEvaluate(i));
+    evaluatedYsAbs(i) = hornerSolve(interpolPolyAbsCoeffs,xToEvaluate(i));
 end
 
-figure;plot(xToEvaluate,evaluatedYs)
+% Plotten des Interpolationspolynoms und von g(x) = |x|
+figure;plot(xToEvaluate,evaluatedYs,xToEvaluate,evaluatedYsAbs)
