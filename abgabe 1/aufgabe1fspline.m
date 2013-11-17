2ef7d01176d70925b798885a951c36e8cef53d77
Inyrie <inyrie@web.de>
2013-11-17 14:30:07 +0100

    Update

--- abgabe 1/aufgabe1fspline.m
+++ abgabe 1/aufgabe1fspline.m
@@ -3,19 +3,21 @@
 
 n=21;
 % Ermitteln der Datenpunkte
-vectorX = -1:0.25:1;
+vectorX = -1:0.001:1;
 vectorY = evaluateOrigFunct(vectorX);
 vectorYAbs = evaluateAbsolute(vectorX);
 
-% MATLAB warnt - Polynomial is badly conditioned!
+% Aufstellen der Splines:
 splineCoeffs = spline(vectorX, vectorY, mycheby(n));
 splineAbsCoeffs = spline(vectorX, vectorYAbs, mycheby(n));
 
+% Auswerten des genaeherten Polynoms an den Chebycheff-Stuetzstellen:
 evaluatedYs = zeros(1,length(vectorX));
-
 for i=1:length(vectorX)
-    evaluatedYs(i) = hornerSolve(interpolPolyCoeffs,vectorX(i));
+    evaluatedYs(i) = hornerSolve(splineCoeffs,vectorX(i));
 end
 
+% Plot der Funktionen:
 figure;plot(vectorX,evaluatedYs)
+%figure;plot(vectorX,evaluate)
 
