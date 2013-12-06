a20e14b166b0a3e67d571e78a989a56805ada99e
Inyrie <inyrie@web.de>
2013-12-06 20:42:47 +0100

    Update

--- abgabe2/AdaptQuad.m
+++ abgabe2/AdaptQuad.m
@@ -1,24 +1,24 @@
-function [ intervallSum ] = AdaptQuad( f, tolerance, leftIntBorder, rightIntBorder )
+function [ intervallSum ] = AdaptQuad( tolerance, leftIntBorder, rightIntBorder )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
 % Bestimmen der Intervallbreite
 h = rightIntBorder - leftIntBorder;
 
-sumPartInt1 = dreiAchtel(f, leftIntBorder, rightIntBorder);
-sumPartInt2 = dreiAchtel(f, leftIntBorder, leftIntBorder+ h/2) + dreiAchtel(f, leftIntBorder + h/2, rightIntBorder);
+sumPartInt1 = dreiAchtel(@f, leftIntBorder, rightIntBorder);
+sumPartInt2 = dreiAchtel(@f, leftIntBorder, (leftIntBorder+ h/2)) + dreiAchtel(@f, (leftIntBorder + h/2), rightIntBorder)
 
-if abs(sumPartInt1 - sumPartInt2) > tolerance
-    
-    % rekursiver Aufruf Formel:
-    % Halbieren von h
-    % Verschieben Intervallgrenzen
-    
-    sumPart1 = AdaptQuad(f, tolerance/2, leftIntBorder, leftIntBorder+ h/2) + AdaptQuad(f, tolerance/2, leftIntBorder + h/2, rightIntBorder);
-    
-end
+% if abs(sumPartInt1 - sumPartInt2) > tolerance
+%     
+%     % rekursiver Aufruf Formel:
+%     % Halbieren von h
+%     % Verschieben Intervallgrenzen
+%     
+%     sumPartInt1 = AdaptQuad(tolerance/2, leftIntBorder, leftIntBorder+ h/2) + AdaptQuad(tolerance/2, leftIntBorder + h/2, rightIntBorder);
+%     
+% end
 
-intervallSum = sumPart1;
+intervallSum = sumPartInt1;
 
 end
 
